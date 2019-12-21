36d9e87ccb7a1adc6c2c68ae144d845f6c74e1f1
Joe Bailey <baileyrva@gmail.com>
2019-12-21 10:11:27 -0500

    Outputing state data

--- states.js
+++ states.js
@@ -12,23 +12,19 @@
 
 
 //Bailey work area 14-35
+$(document).ready(function () {
+    // var state = 'Virginia'
+    var api_url = 'https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest'
+      $.ajax({
+          url: api_url,
+          method: "GET",
+            success: function (current) {
+              var population = show(current)
+              $("#testBox").html(population)
+                function show(current) {
+                    console.log(current.data); 
+                };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                }
+            })
+        });
