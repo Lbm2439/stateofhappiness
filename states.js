7d29118d9752dc98e6cba1031d103faff20dc135
Rob Quinn <Rob.M.Quinn@gmail.com>
2019-12-30 12:16:56 -0500

    updated js file to get the state capital of the state clicked and pull back all breweries in that city

--- states.js
+++ states.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 
-<<<<<<< HEAD
     //THIS IS FOR THE MAP, DONT DELETE
 
     // new Request.JSON({
@@ -16,7 +15,16 @@ $(document).ready(function () {
     //     });
     //END
 
+// cities in State
+    // $.ajax({
+    //     url: 'https://api.census.gov/data/2018/pep/population?get=GEONAME,POP&for=place&in=state:01&key=494e1cd55a3644294c9d5ccdfa0ebba64ee2b477',
+    //     method: "GET"
+    // })
+    //     .then(function (response) {
+    //         console.log(response);
+    //     })
 
+// End Cities in state
 
 
     // function beer(){
@@ -33,25 +41,50 @@ $(document).ready(function () {
        
         // dictionary();
        
-
+// Find capiatl of state clicked and breweries in capital
         $.ajax({
             url: 'https://api.myjson.com/bins/1b1fgk',
             method: "GET"
         })
             .then(function (response) {
-                console.log(response.states[0].name);
+                console.log(response.states);
             })
         
+            $.ajax({
+                url: 'https://api.myjson.com/bins/1b1fgk',
+                method: "GET",
+                success: function(current) {
+                  var capitalData = current.states;
+                  var stateResult = findStateByName(capitalData, stateClicked);
+                  var capital = stateResult.capital;
+                  var currentState = stateResult.State;
+                  console.log(capital);
+                  
+                  var beerURL = "https://api.openbrewerydb.org/breweries?by_state=" + stateClicked + "&by_city=" + capital + "&page=" + page + "&per_page=50&sort=name";
+                  console.log (beerURL);
+                          // var beerURL = "https://api.openbrewerydb.org/breweries?by_city=richmond&by_state=virginia&by_type=micro&page=1&per_page=50&sort=name"
+                          $.ajax({
+                              url: beerURL,
+                              method: "GET"
+                          })
+                              .then(function (response) {
+                                  console.log(response);
+                              })
+                }
+              });
+            // });
+          
+            
+            function findStateByName(capitalData, stateName) {
+              for (var i = 0, len = capitalData.length; i < len; i++) {
+                if (capitalData[i].name === stateName) {
+                  return capitalData[i];
+                }
+              }
+            }
+        
 
-        var beerURL = "https://api.openbrewerydb.org/breweries?by_state=" + stateClicked + "&page=" + page + "&per_page=50&sort=name";
-
-        $.ajax({
-            url: beerURL,
-            method: "GET"
-        })
-            .then(function (response) {
-                console.log(response);
-            })
+     
          
 
         //  function dictionary() {
@@ -202,22 +235,3 @@ console.log(response[0].fl);
                         //       }
                         //     });
                         //   });
-=======
-//THIS IS FOR THE MAP, DONT DELETE
-
-                            new Request.JSON({
-                            url: "map.php",
-                            onSuccess: function(data) {
-                                render(data, {
-                                    editor: false
-                                }); 
-                            }
-                            }).get({
-                                id: 87374,
-                                data: true
-                            });
-              
-//END
-
-
->>>>>>> master
