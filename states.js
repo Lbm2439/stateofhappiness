c818bc7202d9bdb4884b3ac42033b97ea3fb444e
Joe Bailey <baileyrva@gmail.com>
2019-12-30 10:40:07 -0500

    Array for median income

--- states.js
+++ states.js
@@ -2,6 +2,8 @@
 $(document).ready(function() {
   var api_url =
     "https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest";
+  var api_income =
+    "http://datausa.io/api/data?measure=Household%20Income%20by%20Race,Household%20Income%20by%20Race%20Moe&Geography=04000US34:neighbors:parents,04000US34,04000US34:similar";
   $(".hint ").click(function() {
     var foundState = $(this).attr("title");
     $.ajax({
@@ -12,12 +14,19 @@ $(document).ready(function() {
         var stateResult = findStateByName(stateData, foundState);
         var currentPop = stateResult.Population;
         var currentState = stateResult.State;
-        console.log(currentState, currentPop);
+        // console.log(currentState, currentPop);
+      }
+    });
+    $.ajax({
+      url: api_income,
+      method: "GET",
+      success: function(html) {
+        var stateIncome = html.data;
+        console.log(stateIncome);
       }
     });
   });
 
-  
   function findStateByName(stateData, stateName) {
     for (var i = 0, len = stateData.length; i < len; i++) {
       if (stateData[i].State === stateName) {
@@ -25,6 +34,16 @@ $(document).ready(function() {
       }
     }
   }
+
+  function incomeByState(stateIncome, stateName) {
+    for (var i = 0, len = stateIncome.length; i < len; i++) {
+      if (stateIncome[i].Geography === stateName) {
+        return stateIncome[i];
+        
+      }
+    }
+  }
+  
 });
 
 //Bailey work area
